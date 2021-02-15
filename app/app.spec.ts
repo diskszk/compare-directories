2375672906513b90c11bf3d8efb7a4262a27a1b1
diskszk <ibalucky23@gmail.com>
2021-02-15 18:19:37 +0900

    add: add test

---
+++ app/app.spec.ts
@@ -0,0 +1,44 @@
+import {
+  getFilename,
+  checkExistsFileOrDirectory,
+  getOnlyExistsInFormer,
+} from "./app";
+
+const listA = [
+  "/Users/suzukidaisuke/work/dirs/dirA/pokemon/pichu.txt",
+  "/Users/suzukidaisuke/work/dirs/dirA/pokemon/pikachu.txt",
+  "/Users/suzukidaisuke/work/dirs/dirA/pokemon/raichu.txt",
+];
+const listB = [
+  "/Users/suzukidaisuke/work/dirs/dirA/pokemon/pikachu.txt",
+  "/Users/suzukidaisuke/work/dirs/dirA/pokemon/raichu.txt",
+];
+const existsPath = "/Users/suzukidaisuke/work/dirs/dirB/mario/luige.txt";
+const notExistsPath = "/Foo/bar/example/test.txt";
+
+describe("getFilename", () => {
+  test("pathの末尾のファイル名を取得できる", () => {
+    const filename = getFilename(existsPath);
+    expect(filename).toBe("luige.txt");
+  });
+});
+
+describe("checkExistsFileOrDirectory", () => {
+  test("存在するファイルパスを引数に受け取った場合にtrueを返す", () => {
+    const isExists = checkExistsFileOrDirectory(existsPath);
+    expect(isExists).toBe(true);
+  });
+  test("存在しないディレクトリパスを受け立った場合にfalseを返す", () => {
+    const isExists = checkExistsFileOrDirectory(notExistsPath);
+    expect(isExists).toBe(false);
+  });
+});
+
+describe("getOnlyExistsInFormer", () => {
+  test("ListAにのみ存在するパスを取得できる", () => {
+    const onlyExistsListA = getOnlyExistsInFormer(listA, listB);
+    expect(onlyExistsListA).toContain(
+      "/Users/suzukidaisuke/work/dirs/dirA/pokemon/pichu.txt"
+    );
+  });
+});
